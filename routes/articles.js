c9dfba49e795f5daafe028184ad9f74ba5a4d8da
Queenshiba <haruka0111.2a@gmail.com>
2020-06-08 20:47:35 +0900

    add show

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,34 @@
+const ejs = require('ejs')
 const express = require('express')
+
+const Article = require('./../models/article')
 // this gives us a router that we can use to create views
 const router = express.Router()
 
 //
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+    res.render('articles/new', { article: new Article() })
+    // res.send('test')
 })
 
-router.post('/', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if(article == null) res.redirect('/')
+    res.render('article/show', { article: article})
+})
+
+router.post('/', async (req, res) => {
+const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+})
+try {
+    article = await article.save()
+    res.redirect(`/articles/${article.id}`)
+} catch(e) {
+    res.render('articles/new', { article:article })
+}
 
 })
 
--- server.js
+++ server.js
@@ -1,31 +1,42 @@
 // create server, get express from the library
 const express = require('express')
+// import mogodb 
+const mongoose = require('mongoose')
 // make access to article.js
 const articleRouter = require('./routes/articles')
 // calling from express and as function
 const app = express()
 
+// connect to mongodb 
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+
 // set view engine to ejs
 app.set('view engine', 'ejs')
 
-// tell app to use this router
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
+
 
 // create a route, send to the user
 app.get('/', (req,res) => {
+    // res.send('test')
     const articles = [{
         title: 'Test Aritcle',
-        createdAt: new Date(),
+        createdAt: Date.now(),
         description: 'test description'
     },
     {
         title: 'Test Aritcle 2',
-        createdAt:  new Date(),
+        createdAt: Date.now(),
         description: 'test description'
     }]
     // res.send('Hello World') this is addting texts everytime instead
     res.render('articles/index', { articles: articles }) // pass it the path to the view we want
 })
+
+// tell app to use this router
+app.use('/articles', articleRouter)
 
 // pass in a port to start app
 app.listen(5000)
